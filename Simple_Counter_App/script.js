308e874ec26d55b454b0256b863943d6a769cb24
“Allen1303” <132642677+Allen1303@users.noreply.github.com>
2024-12-04 12:03:03 -0500

    Implement a simple counter app with DOM manipulation using Functions to add, subtract and save count that renders to the webpage

---
+++ Simple_Counter_App/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+//Cache DOM 
+const countEl = document.getElementById("count-element");
+const addButton = document.getElementById("add-btn");
+const subtractButton = document.getElementById("subtract-btn");
+const saveButton = document.getElementById("save-btn");
+const previousCount = document.getElementById("previous-count");
+
+/*
+ When the add button is click the count gets updated by one (1) each time.
+ */
+//Global counter variable
+let counter = 0;
+
+//Function declaration syntax
+function addCount() {
+  counter += 1;
+  countEl.textContent = counter;
+}
+//function Expression syntax
+const subtractCount = function () {
+  counter -= 1;
+  countEl.textContent = counter;
+};
+//Arrow Function syntax
+const saveCount = () => {
+  let countStr = counter + " - ";
+  previousCount.textContent += countStr;
+  countEl.textContent = 0;
+  counter = 0;
+};
+//Add button logic to add (1) to count on each click
+addButton.addEventListener("click", addCount);
+//Subtract button logic that subtract (1) from the count on each click
+subtractButton.addEventListener("click", subtractCount);
+//Save button logic that prints the save results to the page
+saveButton.addEventListener("click", saveCount);
