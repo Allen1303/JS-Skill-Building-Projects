150d14cfe5dd31d4ff273d63d3c22836df3e35ff
“Allen1303” <132642677+Allen1303@users.noreply.github.com>
2024-12-12 18:52:36 -0500

    Update JavaScript code

--- Simple_Counter_App/script.js
+++ Simple_Counter_App/script.js
@@ -1,38 +1,37 @@
 "use strict";
 
-//Cache DOM 
+//Cache DOM
 const countEl = document.getElementById("count-element");
 const addButton = document.getElementById("add-btn");
 const subtractButton = document.getElementById("subtract-btn");
 const saveButton = document.getElementById("save-btn");
 const previousCount = document.getElementById("previous-count");
+const container = document.querySelector(".counter-container");
 
 /*
  When the add button is click the count gets updated by one (1) each time.
  */
 //Global counter variable
 let counter = 0;
-
-//Function declaration syntax
-function addCount() {
-  counter ++;
-  countEl.textContent = counter;
-}
-//function Expression syntax
-const subtractCount = function () {
-  counter --;
-  countEl.textContent = counter;
-};
-//Arrow Function syntax
-const saveCount = () => {
-  let countStr = counter + " - ";
-  previousCount.textContent += countStr;
-  countEl.textContent = 0;
-  counter = 0;
-};
-//Add button logic to add (1) to count on each click
-addButton.addEventListener("click", addCount);
-//Subtract button logic that subtract (1) from the count on each click
-subtractButton.addEventListener("click", subtractCount);
-//Save button logic that prints the save results to the page
-saveButton.addEventListener("click", saveCount);
+container.addEventListener("click", (event) => {
+  const targetBtn = event.target;
+  const addCount = targetBtn.dataset.addCount;
+  const subtractCount = targetBtn.dataset.subtractCount;
+  const saveCount = targetBtn.dataset.saveCount;
+  //cognitional statements to update the counter based on the button we click
+  if (addCount) {
+    counter++;
+    countEl.textContent = counter;
+  }
+  if (subtractCount) {
+    counter--;
+    countEl.textContent = counter;
+  }
+  if (saveCount) {
+    let countStr = counter + " - ";
+    previousCount.textContent += countStr;
+    countEl.textContent = 0;
+    counter = 0;
+    
+  }
+});
